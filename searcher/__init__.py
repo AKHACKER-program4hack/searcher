b4fd3792bfa85305d2a92704b14d31cc8d27b278
AKHACKER-program4hack <akhacker.program4hack@gmail.com>
2021-06-28 16:48:56 -0700

    updated

---
+++ searcher/__init__.py
@@ -0,0 +1,10 @@
+
+
+"""
+Testing module for httpimport
+"""
+
+__version__ = '0.3.4'
+__name__ = 'searcher'
+__author__ = 'AK'
+
---
+++ searcher/forsearch.py
@@ -0,0 +1,14 @@
+
+"""
+Searcher module
+"""
+
+
+class Searcher():
+    def __init__(self):
+        self.user = 4
+        
+    def printuser(self):
+        print(self.user)
+        
+
